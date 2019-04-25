e2574ff4429c06a4de7fef6626ca8fc58654fab8
Patrick Thibaudeau <thibaud@yorku.ca>
2019-04-25 12:48:11 -0400

    Updated navigation link

--- lib.php
+++ lib.php
@@ -38,48 +38,50 @@ function local_buildings_extend_navigation(global_navigation $navigation) {
     //Only display if panorama is installed
     $pr_config = $DB->count_records('config_plugins', array('plugin' => 'local_buildings'));
     if ($pr_config > 0) {
-        $node = $navigation->find('local_buildings', navigation_node::TYPE_CONTAINER);
-        if (!$node) {
-            $node = $navigation->add(get_string('pluginname', 'local_buildings'), null, navigation_node::TYPE_CONTAINER, get_string('pluginname', 'local_buildings'), 'local_buildings');
-        }
 
         $context = context_system::instance();
-        //The user can see that IF he has rights on at least one category
-        //profile 4 is the READ ONLY. we don't use the local_buildings::PROFILE_READONLY because it is not loaded here
-
-        $node->add(get_string('dashboard', 'local_buildings'), new moodle_url('/local/buildings/index.php'));
-        $node->showinflatnavigation = true;
+        $node = $navigation->find('local_roomsupport',
+                navigation_node::TYPE_CUSTOM);
+        if (!$node) {
+            $node = $navigation->add(get_string('pluginname',
+                            'local_buildings'),
+                    new moodle_url('/local/buildings/index.php'),
+                    navigation_node::TYPE_CUSTOM,
+                    get_string('pluginname', 'local_buildings'),
+                    'local_buildings');
+            $node->showinflatnavigation = true;
+        }
     }
 }
 
-function local_buildings_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options=array()) {
+function local_buildings_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array()) {
     global $DB;
-    
+
     if ($context->contextlevel != CONTEXT_SYSTEM) {
         return false;
     }
-    
+
     require_login();
-   
+
     if ($filearea !== 'room' && $filearea !== 'image') {
         return false;
     }
-     
-    $itemid = (int)array_shift($args);
+
+    $itemid = (int) array_shift($args);
 
     $fs = get_file_storage();
     $filename = array_pop($args);
-    
+
     if (empty($args)) {
         $filepath = '/';
     } else {
-        $filepath = '/'.implode('/', $args).'/';
+        $filepath = '/' . implode('/', $args) . '/';
     }
-    
+
     $file = $fs->get_file($context->id, 'local_buildings', $filearea, $itemid, $filepath, $filename);
     if (!$file) {
         return false;
     }
-    
+
     send_stored_file($file, 0, 0, $forcedownload, $options);
 }
